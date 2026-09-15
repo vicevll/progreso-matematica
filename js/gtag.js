cc43d2af93722592dede6b34ff49cc9113d8235c
vicevll <70667250+vicevll@users.noreply.github.com>
2026-09-14 21:30:31 -0300

    Agrega Google Analytics (GA4) y ajusta CSP para permitir sus dominios

---
+++ js/gtag.js
@@ -0,0 +1,7 @@
+/* Google Analytics 4 - Studappy */
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-94RBE9QSW2", { send_page_view: true });
